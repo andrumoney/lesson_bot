3499d035b9116d2aa4fa92542b462cb7f1989a5e
andrumoney <aharitonov29@yandex.ru>
2025-05-20 23:44:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 python3 bot.py
+
+import threading
+import http.server
+import socketserver
+
+def keep_alive():
+    PORT = int(os.environ.get("PORT", 8080))
+    handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), handler)
+    threading.Thread(target=httpd.serve_forever, daemon=True).start()
+
+keep_alive()
